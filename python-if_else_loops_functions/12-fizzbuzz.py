0814e16d9ab94405560ea8c1f7dc0f51778d918d
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-22 15:46:24 +0200

    done with all mendatory tasks

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,8 +6,8 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz ", end="")
         else:
-            print(i)
+            print("{}".format(i), end=" ")
