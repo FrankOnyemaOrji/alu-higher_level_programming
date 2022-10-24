f2c48f0265adda86e9cbb2437595ac8a471636aa
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-24 23:54:52 +0200

    done with advanced tasks

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
+    ls = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+    if sys.argv[2] not in list(ls.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if sys.argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ls[sys.argv[2]](a, b)))
