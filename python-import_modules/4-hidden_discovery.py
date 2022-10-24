03287f800d187bf09283f5193f673264572bc9f1
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-24 23:24:59 +0200

    error corrected

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
-    i = dir(hidden_4)
-    i.sort()
-    for j in i:
-        if j[:2] != "_":
-            print(j)
+
+def main():
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print(name)
+
+
+if __name__ == '__main__':
+    main()
