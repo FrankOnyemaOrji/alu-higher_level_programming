33850ade93dba008e55a6cd099f8493fda579de1
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-24 19:21:16 +0200

    done with all mendatory tasks

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 
-import hidden_4.pyc
 if __name__ == "__main__":
     for i in dir(hidden_4):
         if i[0] != '_':
---
+++ python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Add two numbers and return the result.
+    
+    Arg:
+    a = first number
+    b = second number
+
+    Returns:
+    the sum of a and b
+    """
+    return a + b
