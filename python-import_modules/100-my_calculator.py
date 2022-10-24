62e6c7aa03a4aec2b1194c21ee9a76e5165d20aa
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-24 23:45:50 +0200

    done with advanced tasks

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
