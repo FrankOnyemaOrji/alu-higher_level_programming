5ad2d13384d54604a9079c326bc98cb247c992a0
Frank Onyema Orji <f.orji@alustudent.com>
2022-10-31 09:26:10 +0200

    Done with all advanced tasks

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """Computes the square value of all integers of a matrix"""
     return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
