4b425be5d7cb0c571996a53bb1bc95036941661e
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-07 11:31:22 +0200

    DOne with mandatory tasks

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def raise_exception_msg(message=""):
     try:
         raise NameError(message)
