caaab63ebe391057e71df6ddf42a7a2297fdf34c
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-07 11:45:08 +0200

    Done with advanced tasks

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,7 +2,14 @@
 
 
 def magic_calculation(a, b):
-    try:
-        return a ** b
-    except:
-        return None
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return result
