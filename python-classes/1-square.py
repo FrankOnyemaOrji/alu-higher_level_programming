60c230a1f5609d014e99305cf504fde76f604890
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-07 12:21:07 +0200

    Done with mandatory and advanced task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Define a class Square."""
+
 
 class Square:
-    """Represent a square""" 
-    
-    def __init__(self, size=0):
-        """Initialize a new Square
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
         Args:
-            size (int): The size of the new square
+            size (int): The size of the new square.
         """
         self.__size = size
-    
+  
