49a649f7c3a36f31a47d96882099bf4451fb5d2d
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-07 11:35:37 +0200

    DOne with mandatory tasks

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,12 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for i in range(x):
+    count = 0
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
-        print()
-        return x
-    except (ValueError, TypeError):
-        print()
-        return i
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
