8c28ded698c2eca7d4aa3a3c969b473f0734bdf7
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-17 15:02:01 +0200

    done with corrections

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an object that inherits from a list"""
 
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object"""
     return dir(obj)
