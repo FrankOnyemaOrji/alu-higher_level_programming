b88e1cd40916c594c840af6398a02f7beb7cce53
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-21 15:24:41 +0200

    error corrected

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" list the 10 most recents commits on a given GitHub respository"""
-import requests
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
 import sys
+import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],sys.argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    req = requests.get(url)
-    commits = req.json()
+    r = requests.get(url)
+    commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(commits[i].get('sha'), commits[i].get('commit').get('author').get('name')))
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
     
--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" A Python script that:
-- takes in a URL,
-- sends a POST request to the passed URL with the email as a parameter,
-- and displays the body of the response.
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
 """
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-    res = sys.argv[1]
-    email = {'email': sys.argv[2]}
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    req = requests.post(res, data={res, email})
-    print(req.text)
+    r = requests.post(url, data=value)
+    print(r.text)
