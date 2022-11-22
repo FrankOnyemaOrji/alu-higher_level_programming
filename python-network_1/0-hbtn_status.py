f52846c63d399833ed0b4500f5fbbbef109f4ef4
Frank Onyema Orji <f.orji@alustudent.com>
2022-11-22 11:45:23 +0200

    Corrections

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" A Python script
--that fetches https://intranet.hbtn.io/status
--uses urlib package
-"""
-
-
+""" A Python script -that fetches https://intranet.hbtn.io/status -uses urlib package"""
 import urllib.request
-if __name__ == "__main__":
+
+
+if __name__ == "__main_":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
