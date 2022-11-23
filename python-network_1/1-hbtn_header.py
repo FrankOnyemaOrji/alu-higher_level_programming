c84ee5a4e6d085b21a7a56720fb11092f7e47a21
FrankOnyemaOrji <f.orji@alustudent.com>
2022-11-23 20:08:48 +0200

    testing

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,15 +4,10 @@
 -and displays the body
  """
 import sys
-import urllib.request   
-import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode('ascii')
-
-    res = urllib.request.Request(url, data)
-    with urllib.request.urlopen(res) as response:
-        print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html["X-Request-Id"])
