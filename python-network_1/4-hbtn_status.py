a09d4c97d45ab73566b8934e0cf01f42c7edb1ec
FrankOnyemaOrji <f.orji@alustudent.com>
2022-11-24 10:24:54 +0200

    Done

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that fetches data"""
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
