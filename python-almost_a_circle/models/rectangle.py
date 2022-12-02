2eb83ec6af43d9e6e118c07297dd6b1264b51d0c
FrankOnyemaOrji <f.orji@alustudent.com>
2022-12-02 18:55:16 +0200

    done with project almost a circle checcking for errors

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
         print('\n' * self.__y, end='')
         for i in range(self.__height):
             print(' ' * self.__x + '#' * self.__width)
-   
+
     def __str__(self):
         """Return the informal string representation of the rectangle"""
         a = self.id
