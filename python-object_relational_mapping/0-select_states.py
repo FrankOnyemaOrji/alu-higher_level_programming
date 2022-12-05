70c7bd17be49a23d823421779307b5b142e84759
FrankOnyemaOrji <f.orji@alustudent.com>
2022-12-05 10:59:15 +0200

    task one done

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,17 @@ import MySQLdb
 import sys
 
 
+
 def main():
-    """This function lists all states from the database
+    """ lists all states from the database
     hbtn_0e_0_usa"""
-    db = MySQLdb.connect(host="localhost",port=3306, user=sys.argv[1], 
-    passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cur()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
