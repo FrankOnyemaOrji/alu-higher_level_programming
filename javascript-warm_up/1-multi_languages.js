b8b338cfe9b45a77fbf1bdc1390a65b0ecb73c99
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-06 20:05:22 +0200

    Done

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,4 @@
+#!/usr/bin/nod
+const myHeading = "C is fun\nPython is cool\nJavaScript is amazing";
+console.log(myHeading);
+
---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const user = process.argv.slice(2)
+
+if (user.length === 0) {
+  console.log('No argument')
+}else if (user.length === 1) {
+  console.log('Argument found')
+}else {
+	  console.log('Arguments found')
+}
