e72c0ab9afda366305e184f188609cd545d2a0f1
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 11:18:57 +0200

    Task three done

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const user = process.argv.slice(2);
-if (user.length === 0) {
+if (user[0] == null) {
     console.log('No argument');
     }else {
-    console.log('Argument found');
+    console.log(user[0]);
     }
