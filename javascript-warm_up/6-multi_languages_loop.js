9385b4ca0eb9b09186d00ff14d70fa8a11b9395e
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 12:08:22 +0200

    Task six done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const user = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-let i = 0;
-while (i < user.length) {
+for (let i = 0; i < user.length; i++) {
   console.log(user[i]);
-  i++;
 }
