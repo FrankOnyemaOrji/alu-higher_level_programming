0e0a7c9e5d311730aa047037627e508b7d5ac123
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 11:22:35 +0200

    Task three done

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-// const user = process.argv.slice(2);
+const user = process.argv.slice(2);
 
-// if (user.length === 0) {
-//   console.log('No argument');
-// } else if (user.length === 1) {
-//   console.log('Argument found');
-// } else {
-//   console.log('Arguments found');
-// }
-
-
-const user =  "name"
-
-if (user === "name"){
-  console.log("Argumnet found")
-} else if (user != "name"){
-  console.log("No argument")
+if (user.length === 0) {
+  console.log('No argument');
+} else if (user.length === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found")
+  console.log('Arguments found');
 }
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const user = process.argv.slice(2);
 if (user[0] == null) {
-    console.log('No argument');
-    }else {
-    console.log(user[0]);
-    }
+  console.log('No argument');
+} else {
+  console.log(user[0]);
+}
