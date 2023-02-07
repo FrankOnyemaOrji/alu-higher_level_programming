1ac1a1e273ab0ace38b3aebc188e6c475be5acba
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 11:51:57 +0200

    Done with task five

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const user = process.argv.slice(2);
 // Converting the string to an integer
-if (parseInt(user[0])) {
+if (isNaN(parseInt(user[0]))) {
   console.log('My number: ' + user[0]);
 } else {
   console.log('My number: ' + Number(user[0]));
