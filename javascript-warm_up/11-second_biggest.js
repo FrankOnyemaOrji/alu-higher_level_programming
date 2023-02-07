5e73059c6f258e1b39488916931349e0e95521e9
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 13:05:18 +0200

    Done with task eleven

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const user = process.argv.slice(2);
+const a = [];
+switch (user) {
+  case 2:
+  case 3:
+    console.log(0);
+    break;
+
+  default:
+    for (let i = 0; i < user.length; i++) {
+      a.push(parseInt(user[i]));
+    }
+    a.sort((a, b) => b - a);
+    console.log(a[1]);
+    break;
+}
