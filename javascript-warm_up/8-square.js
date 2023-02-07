adecbb039eaedc6255bd78a649f58264df3ce6de
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-07 12:45:27 +0200

    Done with task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const user = process.argv.slice(2);
-//converting first argument to an integer
+// converting first argument to an integer
 if (isNaN(parseInt(user[0]))) {
   console.log('Missing size');
 } else {
-    const square = 'X'.repeat(parseInt(user[0]));
-    let i = 0;
-    while (i < user[0]) {
-        console.log('X'.repeat(user[0]));
-        i++;
-    }
-    }
+  let i = 0;
+  while (i < user[0]) {
+    console.log('X'.repeat(user[0]));
+    i++;
+  }
+}
