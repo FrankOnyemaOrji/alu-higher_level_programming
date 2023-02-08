88cd16c4c8c71934d6b844c3473c7c6754b78ea2
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-08 07:55:24 +0200

    Done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const user = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const user = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < user.length) {
   console.log(user[i]);
