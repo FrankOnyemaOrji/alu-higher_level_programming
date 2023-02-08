82d4b21c829a803248f4c78e504e49d95f8bf3e7
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-08 08:28:26 +0200

    Done with task nine

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const usera = 1;
-const userb = 2;
 function add (a, b) {
-  console.log(a, b);
+  console.log(a + b);
 }
 
-add(Number(usera[0]), Number(user[1]));
+add(Number(process.argv[2]), Number(process.argv[3]));
