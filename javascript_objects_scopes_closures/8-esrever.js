53c55b905a202276d67af582adda22afaeda6646
FrankOnyemaOrji <f.orji@alustudent.com>
2023-02-09 17:43:23 +0200

    Done with task eight till ten

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const rev = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-        rev.push(list[i]);
-    }
-    return rev;
-}
+  const rev = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+  return rev;
+};
